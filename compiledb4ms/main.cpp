91a6a25be091cfb551f3f0699659cc6dcdeff6f2
Nihility <Nihility.t@gmail.com>
2025-07-30 20:44:54 +0800

    feat: extract compile file path of command object from vcxproj

--- compiledb4ms/main.cpp
+++ compiledb4ms/main.cpp
@@ -15,8 +15,15 @@ int main()
 		Command_object obj = {
 			"D:/!dev/WIP/tools/toolchain/compiledb4ms/build",
 			cl_compile_file,
-			R"("D:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Tools\MSVC\14.43.34808\bin\HostX64\x64\CL.exe" /c /Zi /nologo /W4 /WX /diagnostics:column /Od /Ob0 /D _MBCS /D WIN32 /D _WINDOWS /D GTEST_LINKED_AS_SHARED_LIBRARY=1 /D "CMAKE_INTDIR=\"Debug\"" /EHsc /RTC1 /MDd /GS /fp:precise /std:c++17 /Fo"acceptance_test.dir\Debug\\" /Fd"acceptance_test.dir\Debug\vc143.pdb" /external:W0 /Gd /TP /errorReport:queue  /external:I "D:/!dev/WIP/tools/toolchain/compiledb4ms/build/vcpkg_installed/x64-windows/include" "D:\!dev\WIP\tools\toolchain\compiledb4ms\tests\usage.cpp")",
+			R"("D:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Tools\MSVC\14.43.34808\bin\HostX64\x64\CL.exe" )"
+			R"(/c /Zi /nologo /W4 /WX /diagnostics:column /Od /Ob0 )"
+			R"(/D _MBCS /D WIN32 /D _WINDOWS /D GTEST_LINKED_AS_SHARED_LIBRARY=1 /D "CMAKE_INTDIR=\"Debug\"" )"
+			R"(/EHsc /RTC1 /MDd /GS /fp:precise /std:c++17 /Fo"acceptance_test.dir\Debug\\" )"
+			R"(/Fd"acceptance_test.dir\Debug\vc143.pdb" /external:W0 /Gd /TP /errorReport:queue )"
+			R"( /external:I "D:/!dev/WIP/tools/toolchain/compiledb4ms/build/vcpkg_installed/x64-windows/include" )"
+			"\"" + cl_compile_file.string() + '"',
 		};
+
 		out << obj.str();
 	}
 	out << "\n]";
