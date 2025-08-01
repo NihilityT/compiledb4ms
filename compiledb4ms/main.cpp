1c24964d709d4b4553489f28ceec8dcf7b24cdd8
Nihility <Nihility.t@gmail.com>
2025-08-02 01:47:23 +0800

    feat: support passing arch type via command line

--- compiledb4ms/main.cpp
+++ compiledb4ms/main.cpp
@@ -15,16 +15,22 @@ int main(int argc, char* argv[])
 		return -1;
 	}
 
+	std::string_view arch_type;
+	int out_count = 0;
+
 	std::ofstream out{ "compile_commands.json" };
 	out << "[\n";
 	for (int i = 1; i < argc; i += 2) {
-		if (i > 1) {
-			out << ",\n";
-		}
-
-		if (argv[i] == "-p"sv) {
-			Vcxproj proj{ argv[i + 1] };
+		if (argv[i] == "-t"sv) {
+			arch_type = argv[i + 1];
+		} else if (argv[i] == "-p"sv) {
+			Vcxproj proj = arch_type.empty()
+				? Vcxproj{ argv[i + 1] }
+				: Vcxproj{ argv[i + 1], arch_type };
 			for (auto& obj : proj.command_objects()) {
+				if (out_count++) {
+					out << ",\n";
+				}
 				out << obj;
 			}
 		}
--- tests/usage.cpp
+++ tests/usage.cpp
@@ -25,3 +25,21 @@ TEST(Usage, generate_compiledb_for_vcxproj)
 // original
 // D:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Tools\MSVC\14.43.34808\bin\HostX64\x64\CL.exe /c /Zi /nologo /W4 /WX /diagnostics:column /Od /Ob0 /D _MBCS /D WIN32 /D _WINDOWS /D GTEST_LINKED_AS_SHARED_LIBRARY=1 /D "CMAKE_INTDIR=\"Debug\"" /EHsc /RTC1 /MDd /GS /fp:precise /std:c++17 /Fo"acceptance_test.dir\Debug\\" /Fd"acceptance_test.dir\Debug\vc143.pdb" /external:W0 /Gd /TP /errorReport:queue  /external:I "D:/!dev/WIP/tools/toolchain/compiledb4ms/build/vcpkg_installed/x64-windows/include" "D:\!dev\WIP\tools\toolchain\compiledb4ms\tests\usage.cpp"
 }
+
+TEST(Usage, generate_compiledb_for_vcxproj_with_release_x64_type)
+{
+	std::string compiledb4ms = utils::get_env("compiledb4ms", "compiledb4ms");
+	std::string cmd = std::string(compiledb4ms) + R"( -t "Release|x64" -p tests/usage.vcxproj)";
+	int exit_code = system(cmd.c_str());
+	ASSERT_EQ(0, exit_code);
+
+	auto res = utils::read_file("compile_commands.json");
+	std::string expected = R"([
+{
+  "directory": "D:\\!dev\\WIP\\tools\\toolchain\\compiledb4ms\\tests",
+  "file": "D:\\!dev\\WIP\\tools\\toolchain\\compiledb4ms\\tests\\usage.cpp",
+  "command": "\"D:\\Program Files\\Microsoft Visual Studio\\2022\\Enterprise\\VC\\Tools\\MSVC\\14.43.34808\\bin\\Hostx64\\x64\\cl.exe\" /c  /nologo /W4 /WX /diagnostics:column /O2 /Ob2 /D _MBCS /D WIN32 /D _WINDOWS /D NDEBUG /D GTEST_LINKED_AS_SHARED_LIBRARY=1 /D \"CMAKE_INTDIR=\\\"Release\\\"\" /EHsc  /MD /GS /fp:precise /std:c++17 /Fo\"acceptance_test.dir\\Release\\\\\" /Fd\"acceptance_test.dir\\Release\\vc143.pdb\" /external:W0 /Gd /TP /errorReport:queue  /external:I \"D:/!dev/WIP/tools/toolchain/compiledb4ms/build/vcpkg_installed/x64-windows/include\" \"D:\\!dev\\WIP\\tools\\toolchain\\compiledb4ms\\tests\\usage.cpp\""
+}
+])";
+	ASSERT_EQ(res, expected);
+}
